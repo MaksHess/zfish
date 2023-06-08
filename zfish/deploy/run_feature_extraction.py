c2e97ca0146bcd5d9cefce03630770280514ae7a
MaksHess <max.hess@imls.uzh.ch>
2023-06-08 13:02:19 +0200

    logs

--- zfish/deploy/run_feature_extraction.py
+++ zfish/deploy/run_feature_extraction.py
@@ -9,8 +9,8 @@ SLURM_COMMAND = """#!/usr/bin/env bash
 #SBATCH --array=0-{0}%50
 #SBATCH --mem-per-cpu=10000m
 #SBATCH --cpus-per-task=2
-#SBATCH --error=logs/%A_%a.err
-#SBATCH --output=logs/%A_%a.out
+#SBATCH --error=log.err
+#SBATCH --output=log.out
 #SBATCH --time=3-00:00:00
 
 source ~/.bashrc

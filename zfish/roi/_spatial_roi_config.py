b17e193cf47e6b74ec5d0d2265db56a404b0f811
MaksHess <max.hess@imls.uzh.ch>
2023-07-05 15:20:11 +0200

    add index order

--- zfish/roi/_spatial_roi_config.py
+++ zfish/roi/_spatial_roi_config.py
@@ -8,6 +8,7 @@ from typing import Sequence
 class SortKey:
     label_order: Sequence[str] = ('^emb.*$', '^cell.*$', '^nuc.*$', '^cyto.*$', '^mem.*$', '^loc.*$')
     dim_order: Sequence[str] = ('roi', 'l', 'c', 't', 'z', 'y', 'x')
+    index_order: Sequence[str] = ('roi', 'object', 'structure', 'label', 'channel')
 
     def labels(self, label_type: str) -> int:
         if self.label_order[0].startswith('^') and self.label_order[0].endswith('$'):
@@ -19,14 +20,20 @@ class SortKey:
         else:
             return self.label_order.index(label_type)
         
-    def images(self, channel_type: str) -> tuple[int, str]:
+    def images(self, channel_type: str, sep: str = '.') -> tuple[int, str]:
         try:
-            stain = '-'.join(channel_type.split('-')[:-1])
-            acquisition = int(channel_type.split('-')[-1])
+            stain = sep.join(channel_type.split(sep)[:-1])
+            acquisition = int(channel_type.split(sep)[-1])
             return acquisition, stain
         except ValueError as e:
-            warnings.warn(f"`{channel_type}` must end in `-\\d+`, returning `(1000, '')`")
+            warnings.warn(f"`{channel_type}` must end in `{sep}\\d+`, returning `(1000, '')`")
             return (1000, '')
         
     def dims(self, dim: str) -> int:
         return self.dim_order.index(dim)
+    
+    def idx(self, idx_column: str) -> int:
+        try:
+            return self.index_order.index(idx_column)
+        except ValueError:
+            return 1000

9a6372cb693fd9b36a89b44c3ef4ee3559c41850
MaksHess <max.hess@imls.uzh.ch>
2023-10-21 01:03:56 +0200

    delete some files

--- zfish/features/types.py
+++
@@ -1,8 +0,0 @@
-from typing import TypeAlias
-
-from spatial_image import SpatialImage
-
-LabelImage: TypeAlias = SpatialImage
-MultichannelLabelImage: TypeAlias = SpatialImage
-BinaryImage: TypeAlias = SpatialImage
-DistanceTransform: TypeAlias = SpatialImage

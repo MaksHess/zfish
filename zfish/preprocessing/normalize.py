f2ca5e134bacc8beda53c0ccbc2092025627302c
MaksHess <max.hess@imls.uzh.ch>
2024-02-21 15:11:24 +0100

    add preprocessing

---
+++ zfish/preprocessing/normalize.py
@@ -0,0 +1,103 @@
+# %%
+import logging
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Literal, Sequence
+
+import polars as pl
+import polars.selectors as cs
+from polars.type_aliases import SelectorType
+from zfish.features.polars_utils import drop_null_columns, id_, log
+from zfish.preprocessing.outlier_ranges import (
+    INTENSITY_OUTLIERS,
+    SEGMENTATION_OUTLIERS,
+    Outlier,
+    clip_outliers,
+    remove_outliers,
+)
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass(frozen=True)
+class OutlierParams:
+    strategy: Literal["skip", "drop", "clip"] = "drop"
+    outliers: Sequence[Outlier] = tuple(
+            SEGMENTATION_OUTLIERS + INTENSITY_OUTLIERS
+    )
+    verbose: bool = True
+    plot_results: bool = False
+
+
+@dataclass(frozen=True)
+class NullHandlingParams:
+    strategy: Literal[
+        "skip", "drop_feature_rows", "drop_cols", "drop_all"
+    ] = "drop_all"
+    verbose: bool = True
+    features: SelectorType = cs.all()
+
+
+def handle_nulls(
+    df,
+    strategy: str = "drop_cols",
+    verbose: bool = True,
+    features: SelectorType = cs.all(),
+) -> pl.DataFrame:
+    if verbose:
+        shape_logger = log
+    else:
+        shape_logger = id_
+        
+    features_exp = cs.expand_selector(df, features)
+    
+    shape_logger(df, message="before:")
+    if strategy == "skip":
+        return df.fill_nan(None).pipe(shape_logger, message="after:")
+    elif strategy == "drop_cols":
+        return (
+            df.fill_nan(None)
+            .pipe(drop_null_columns, strategy='any', columns=features)
+            .pipe(shape_logger, message="after:")
+        )
+    elif strategy == "drop_rows":
+        return df.fill_nan(None).drop_nulls().pipe(shape_logger, message="after:")
+    elif strategy == "drop_feature_rows":
+        return (
+            df.fill_nan(None)
+            .drop_nulls(subset=features_exp)
+            .pipe(shape_logger, message="after:")
+        )
+    elif strategy == "drop_all":
+        return (
+            df.fill_nan(None)
+            .pipe(drop_null_columns, strategy='any', columns=features)
+            .drop_nulls()
+            .pipe(shape_logger, message="after:")
+        )
+    else:
+        raise RuntimeError(f"Unknown null strategy {strategy}")
+
+
+def handle_outliers(df, strategy='drop', outliers: Sequence[Outlier] = tuple(), verbose: bool = True, plot_results: bool=False) -> pl.DataFrame:
+    print(outliers)
+    if strategy == "skip":
+        return df
+    elif strategy == "drop":
+        return df.pipe(
+            remove_outliers,
+            outliers=outliers,
+            verbose=verbose,
+            plot_results=plot_results,
+        )
+    elif strategy == "clip":
+        return df.pipe(
+            clip_outliers,
+            outliers=outliers,
+            verbose=verbose,
+            plot_results=plot_results,
+        )
+    else:
+        raise RuntimeError(f"Unknown outlier strategy {strategy}")
+
+
+# %%
---
+++ zfish/preprocessing/outlier_ranges.py
@@ -0,0 +1,485 @@
+# %%
+import logging
+from dataclasses import dataclass
+from itertools import zip_longest
+from typing import Literal, Protocol, Sequence, runtime_checkable
+
+import numpy as np
+import polars as pl
+
+from zfish.plot.features import iqr_range
+from zfish.preprocessing.types import AnyFrameT
+
+logger = logging.getLogger(__name__)
+
+
+
+def identity(x):
+    return x
+
+TRANSFORMS = {
+    'log10': np.log10,
+    'log2': np.log2,
+    'log': np.log,
+    'identity': identity,
+}
+
+
+SEGMENTATION_OUTLIER_RANGES = [
+    {
+        "feature": "EquivalentSphericalRadius",  # um
+        "lower": 4,
+        "upper": 8,
+    },
+]
+INTENSITY_OUTLIER_RANGES = [
+    {
+        "feature": "PCNA.0_Mean",
+        "lower": 1.0,
+        "upper": 10.0,
+    },
+    {
+        "feature": "PCNA.0_Skewness",
+        "upper": 3.0,
+    },
+    {
+        "feature": "PCNA.0_Kurtosis",
+        "upper": 5.0,
+    },
+    {
+        "feature": "DAPI.1_Mean",
+        "lower": 1.0,
+        "upper": 8.0,
+    },
+    {
+        "feature": "DAPI.1_Skewness",
+        "lower": -2.0,
+        "upper": 3.0,
+    },
+    {
+        "feature": "DAPI.1_Kurtosis",
+        "upper": 5.0,
+    },
+    {
+        "feature": "pH3.1_Mean",
+        "lower": 1.0,
+        "upper": 8.0,
+    },
+    {
+        "feature": "pH3.1_Variance",
+        "lower": 0.0,
+        "upper": 14.0,
+    },
+    {
+        "feature": "pH3.1_Kurtosis",
+        "upper": 5.0,
+    },
+]
+
+DEFAULT_OUTLIER_RANGES = SEGMENTATION_OUTLIER_RANGES + INTENSITY_OUTLIER_RANGES
+
+
+@runtime_checkable
+class Outlier(Protocol):
+    feature: str
+    transform: Literal['log10', 'log2', 'log', 'identity']
+
+    def fit(self, df: pl.DataFrame) -> "Outlier":
+        ...
+
+    @property
+    def lower(self) -> str | None:
+        ...
+
+    @property
+    def upper(self) -> str | None:
+        ...
+
+    # def indicate_lower_outlier(self, df: pl.DataFrame) -> pl.Series:
+    #     self.fit(df)
+    #     if self.lower is None:
+    #         return df.select(pl.col(self.feature).is_null()).to_series()
+    #     return df.select(pl.col(self.feature).lt(self.lower)).to_series()
+    
+    def indicate_lower_outlier(self, df: pl.DataFrame) -> pl.Series:
+        self.fit(df)
+        col_expression = pl.col(self.feature).map_elements(TRANSFORMS[self.transform])
+        if self.lower is None:
+            return df.select(col_expression.is_null()).to_series()
+        return df.select(col_expression.lt(self.lower)).to_series()
+
+    # def indicate_upper_outlier(self, df: pl.DataFrame) -> pl.Series:
+    #     self.fit(df)
+    #     if self.upper is None:
+    #         return df.select(pl.col(self.feature).is_null()).to_series()
+    #     return df.select(pl.col(self.feature).gt(self.upper)).to_series()
+    
+    def indicate_upper_outlier(self, df: pl.DataFrame) -> pl.Series:
+        self.fit(df)
+        col_expression = pl.col(self.feature).map_elements(TRANSFORMS[self.transform])
+        if self.upper is None:
+            return df.select(col_expression.is_null()).to_series()
+        return df.select(col_expression.gt(self.upper)).to_series()
+
+    def indicate_outlier(self, df: pl.DataFrame) -> pl.Series:
+        return (
+            self.indicate_lower_outlier(df)
+            | self.indicate_upper_outlier(df)
+        ).to_frame()
+
+    def __str__(self) -> str:
+        return self.__repr__()
+
+
+@dataclass
+class RngOutlier(Outlier):
+    feature: str
+    lower: float | None = None
+    upper: float | None = None
+    transform: Literal['log10', 'log2', 'log', 'identity'] = 'identity'
+
+    def fit(self, df):
+        return self
+
+
+@dataclass
+class QuantOutlier(Outlier):
+    feature: str
+    q_lower: float = 0.0
+    q_upper: float = 1.0
+    transform: Literal['log10', 'log2', 'log', 'identity'] = 'identity'
+
+    def fit(self, df):
+        self._lower = df[self.feature].quantile(self.q_lower)
+        self._upper = df[self.feature].quantile(self.q_upper)
+
+    @property
+    def lower(self) -> float:
+        if not hasattr(self, "_lower"):
+            raise AttributeError("Call fit() first")
+        return self._lower
+
+    @property
+    def upper(self) -> float:
+        if not hasattr(self, "_upper"):
+            raise AttributeError("Call fit() first")
+        return self._upper
+
+
+@dataclass
+class IQROutlier(Outlier):
+    feature: str
+    q_lower: float | None = 0.1
+    q_upper: float | None = 0.9
+    iqr_mult: float = 2.0
+    transform: Literal['log10', 'log2', 'log', 'identity'] = 'identity'
+
+    def fit(self, df):
+        if not self.transform == 'identity':
+            raise NotImplementedError("IQROutlier on transformed feature columnes not yet implemented!")
+        if self.q_lower is None and self.q_upper is None:
+            self._lower = df.select(pl.col(self.feature).min())[self.feature].item()
+            self._upper = df.select(pl.col(self.feature).max())[self.feature].item()
+
+        elif self.q_lower is None:
+            self._lower = df.select(pl.col(self.feature).min())[self.feature].item()
+            upper = df.select(pl.col(self.feature).quantile(self.q_upper))[
+                self.feature
+            ].item()
+            iqr = upper - self._lower
+            self._upper = upper + (self.iqr_mult - 1) * iqr
+
+        elif self.q_upper is None:
+            self._upper = df.select(pl.col(self.feature).max())[self.feature].item()
+            lower = df.select(pl.col(self.feature).quantile(self.q_lower))[
+                self.feature
+            ].item()
+            iqr = self._upper - lower
+            self._lower = lower - (self.iqr_mult - 1) * iqr
+
+        else:
+            lower = df.select(pl.col(self.feature).quantile(self.q_lower))[
+                self.feature
+            ].item()
+            upper = df.select(pl.col(self.feature).quantile(self.q_upper))[
+                self.feature
+            ].item()
+            iqr = upper - lower
+            self._lower = lower - (self.iqr_mult - 1) / 2 * iqr
+            self._upper = upper + (self.iqr_mult - 1) / 2 * iqr
+
+    @property
+    def lower(self) -> str | None:
+        if not hasattr(self, "_lower"):
+            raise AttributeError("Call fit() first")
+        return self._lower
+
+    @property
+    def upper(self) -> str | None:
+        if not hasattr(self, "_upper"):
+            raise AttributeError("Call fit() first")
+        return self._upper
+
+SEGMENTATION_OUTLIERS = tuple(RngOutlier(**rng) for rng in SEGMENTATION_OUTLIER_RANGES)
+INTENSITY_OUTLIERS = tuple(RngOutlier(**rng) for rng in INTENSITY_OUTLIER_RANGES)
+
+def mark_outliers(df: AnyFrameT, outliers: list[Outlier], verbose=True) -> pl.Series:
+    is_outlier = pl.concat(
+        [outlier.indicate_outlier(df) for outlier in outliers],
+        how="horizontal",
+    ).with_columns(
+        pl.all_horizontal(pl.all()).alias("All Outliers"),
+        pl.any_horizontal(pl.all()).alias("Any Outliers"),
+    )
+    if verbose:
+        is_below = pl.DataFrame(
+            [outlier.indicate_lower_outlier(df) for outlier in outliers],
+        ).with_columns(
+            pl.all_horizontal(pl.all()).alias("All Outliers"),
+            pl.any_horizontal(pl.all()).alias("Any Outliers"),
+        )
+        is_above = pl.DataFrame(
+            [outlier.indicate_upper_outlier(df) for outlier in outliers],
+        ).with_columns(
+            pl.all_horizontal(pl.all()).alias("All Outliers"),
+            pl.any_horizontal(pl.all()).alias("Any Outliers"),
+        )
+    else:
+        is_below = is_above = None
+
+    outlier_summary(is_outlier, is_below, is_above)
+    return is_outlier.select(pl.col('Any Outliers')).to_series()
+
+
+def outlier_summary(
+    df_outliers: pl.DataFrame, is_below=None, is_above=None
+) -> pl.DataFrame:
+    if hasattr(df_outliers, "to_frame"):
+        df_outliers = df_outliers.to_frame()
+    n_total = df_outliers.height
+    for c in df_outliers.columns:
+        n_outliers = df_outliers[c].sum()
+        logger.info(f"{c}")
+        logger.info(
+            f"{n_outliers:>8,}/{n_total:<8,}={n_outliers/n_total:>7.2%} outliers or null"
+        )
+        if is_below is not None:
+            n_below = is_below[c].sum()
+            logger.info(f"{n_below:>8,}/{n_total:<8,}={n_below/n_total:>7.2%} below")
+        if is_above is not None:
+            n_above = is_above[c].sum()
+            logger.info(f"{n_above:>8,}/{n_total:<8,}={n_above/n_total:>7.2%} above")
+
+
+def remove_outliers(df, outliers: Sequence[Outlier], verbose=True, plot_results=False):
+    df_outliers = mark_outliers(df, outliers, verbose=verbose)
+    if verbose:
+        logger.info(f"{' Removing outliers ':=^50}")
+        outlier_summary(df_outliers)
+    df_out = df.filter(~df_outliers)
+    if plot_results:
+        results_plot(df, df_out, outliers)
+    return df_out
+
+
+def clip_outliers(df, outliers: Sequence[Outlier], verbose=True, plot_results=False):
+    if verbose:
+        df_outliers = mark_outliers(df, outliers, verbose=verbose)
+        logger.info(f"{' Clipping values to outlier ranges ':=^50}")
+        outlier_summary(df_outliers)
+    df_out = df.clone()
+    for outlier in outliers:
+        outlier.fit(df)
+        if outlier.lower is not None:
+            df_out = df_out.with_columns(pl.col(outlier.feature).clip_min(outlier.lower))
+        if outlier.upper is not None:
+            df_out = df_out.with_columns(pl.col(outlier.feature).clip_max(outlier.upper))
+    if plot_results:
+        results_plot(df, df_out, outliers)
+    return df_out
+
+
+
+def results_plot(df, df_out, outliers: Sequence[Outlier], ncols=4, figsize=None):
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import seaborn as sns
+    last_axes = False
+    nrows = int(np.ceil(len(outliers) / ncols))
+    figsize = figsize or (ncols * 4, nrows * 3)
+    fig, axs = plt.subplots(ncols=ncols, nrows=nrows, figsize=figsize, dpi=300)
+    old_ax = axs[0]
+    for outlier, ax in zip_longest(outliers, axs.flatten()):
+        if outlier is None:
+            ax.remove()
+            if last_axes is False:
+                last_axes is True
+                old_ax.legend()
+                continue
+        sns.kdeplot(df[outlier.feature], ax=ax, label="before")
+        sns.kdeplot(df_out[outlier.feature], ax=ax, label="after")
+        ax.set_xlim(*iqr_range(df[outlier.feature], q_lower=0.1, q_upper=0.9, r=3))
+        ax.set_title(outlier.feature)
+        if outlier.lower is not None:
+            ax.axvline(x=outlier.lower, color='k', linestyle='dotted')
+        if outlier.upper is not None:
+            ax.axvline(x=outlier.upper, color='k', linestyle='dashed')
+        
+        old_ax = ax
+
+def main():
+    df = pl.DataFrame({'a': np.arange(1, 11, dtype=np.float32)})
+    outlier = RngOutlier(feature='a', lower=3, upper=6, transform='identity')
+    log_outlier = RngOutlier(feature='a', lower=np.log(3), upper=np.log(6), transform='log')
+    log10_outlier = RngOutlier(feature='a', lower=np.log10(3), upper=np.log10(6), transform='log10')
+    log2_outlier = RngOutlier(feature='a', lower=np.log2(3), upper=np.log2(6), transform='log2')
+    
+    assert np.all(outlier.indicate_outlier(df) == log_outlier.indicate_outlier(df))
+    assert np.all(outlier.indicate_outlier(df) == log10_outlier.indicate_outlier(df))
+    assert np.all(outlier.indicate_outlier(df) == log2_outlier.indicate_outlier(df))
+
+
+# def mark_range_outlier(
+#     df: AnyFrameT,
+#     feature: str,
+#     lower: float | None = None,
+#     upper: float | None = None,
+# ) -> pl.DataFrame:
+#     if lower is None and upper is None:
+#         name = f"{feature}_range_outlier"
+#         filter_expr = pl.col(feature).is_null()
+#     elif lower is None:
+#         name = f"{feature}_u_range_outlier"
+#         filter_expr = ~pl.col(feature).le(upper)
+#     elif upper is None:
+#         name = f"{feature}_l_range_outlier"
+#         filter_expr = ~pl.col(feature).ge(lower)
+#     else:
+#         name = f"{feature}_lu_range_outlier"
+#         filter_expr = ~pl.col(feature).is_between(lower, upper)
+
+#     is_outlier = df.select(pl.when(filter_expr).then(True).otherwise(False).alias(name))
+#     return is_outlier
+
+
+# def mark_range_outliers(
+#     df: AnyFrameT, outlier_ranges: tuple[dict[str, Any]]
+# ) -> pl.DataFrame:
+#     return pl.concat(
+#         [mark_range_outlier(df, **rng) for rng in outlier_ranges], how="horizontal"
+#     ).with_columns(
+#         pl.all_horizontal(pl.all()).alias("all_range_outlier"),
+#         pl.any_horizontal(pl.all()).alias("any_range_outlier"),
+#     )
+
+
+# def discard_bounds(
+#     df: AnyFrameT,
+#     feature: str,
+#     lower: float | None = None,
+#     upper: float | None = None,
+#     verbose: bool = True,
+#     plot_results: bool = True,
+#     ax=None,
+# ) -> AnyFrameT:
+#     if lower is None and upper is None:
+#         out = df
+#     elif lower is None:
+#         x = [upper]
+#         linestyles = ["dashed"]
+#         out = df.filter(pl.col(feature).lt(upper))
+#     elif upper is None:
+#         x = [lower]
+#         linestyles = ["dotted"]
+#         out = df.filter(pl.col(feature).gt(lower))
+#     else:
+#         x = [lower, upper]
+#         linestyles = ["dotted", "dashed"]
+#         out = df.filter(pl.col(feature).is_between(lower, upper))
+
+#     if verbose or plot_results:
+#         if isinstance(df, pl.LazyFrame):
+#             df: pl.DataFrame = df.collect()
+#             out: pl.DataFrame = out.collect()
+#             return_lazy = True
+#         else:
+#             return_lazy = False
+
+#     if verbose:
+#         n_obs_before = df.height
+#         n_obs_after = out.height
+#         logger.info(
+#             f"{n_obs_after}/{n_obs_before} ({n_obs_after/n_obs_before:2.2%}) remaining \
+# objs after {feature!r}"
+#         )
+
+#     if plot_results:
+#         import matplotlib.pyplot as plt
+#         import seaborn as sns
+
+#         from zfish.features.polars_selector import sel
+
+#         if ax is None:
+#             fig, ax = plt.subplots(figsize=(4, 3))
+#         ax = sns.kdeplot(
+#             df.select(
+#                 [
+#                     sel.index,
+#                     pl.col(feature),
+#                 ]
+#             ).to_pandas(),
+#             x=feature,
+#             ax=ax,
+#         )
+
+#         y_lims = ax.get_ylim()
+#         plt.vlines(
+#             x=x,
+#             ymin=y_lims[0],
+#             ymax=y_lims[1],
+#             colors=["k"],
+#             linestyles=linestyles,
+#         )
+
+#     if return_lazy:
+#         return out.lazy()
+#     else:
+#         return out
+
+
+# class OutlierRange(TypedDict):
+#     feature: str
+#     lower: float | None
+#     upper: float | None
+
+
+# def remove_outliers(
+#     df: AnyFrameT, outliers: list[OutlierRange], verbose=True, plot_results=True
+# ) -> AnyFrameT:
+#     if verbose:
+#         if isinstance(df, pl.LazyFrame):
+#             n_obs_initial = df.collect().height
+#         else:
+#             n_obs_initial = df.height
+
+#     for outlier in outliers:
+#         df = partial(
+#             discard_bounds, **outlier, verbose=verbose, plot_results=plot_results
+#         )(df)
+
+#     if verbose:
+#         if isinstance(df, pl.LazyFrame):
+#             n_obs_final = df.collect().height
+#         else:
+#             n_obs_final = df.height
+#         logger.info(
+#             f"{n_obs_final}/{n_obs_initial} ({n_obs_final/n_obs_initial:2.2%}) remaining \
+# objs in the end."
+#         )
+
+#     return df
+
+
+
+
+# # %%

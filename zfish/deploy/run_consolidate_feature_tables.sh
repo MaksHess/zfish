05e13c4f7315f5be62902ddac52e8f08f5bfee9f
Max Timo Hess <hmax@pelkmanslab-slurm-master-001>
2024-03-27 23:17:27 +0000

    more memory for deltalake writing

--- zfish/deploy/run_consolidate_feature_tables.sh
+++ zfish/deploy/run_consolidate_feature_tables.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 #SBATCH --array=9-17
-#SBATCH --mem-per-cpu=10000m
-#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=20000m
+#SBATCH --cpus-per-task=4
 #SBATCH --error=./logs/slurm-%A_%a.err
 #SBATCH --output=./logs/slurm-%A_%a.out
 #SBATCH --time=3-00:00:00

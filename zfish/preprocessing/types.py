e91beeb46aec098fe633c143068360f98e7248fa
MaksHess <max.hess@imls.uzh.ch>
2024-03-31 23:10:20 +0200

    type alias FrameOrLazy

--- zfish/preprocessing/types.py
+++ zfish/preprocessing/types.py
@@ -1,5 +1,6 @@
-from typing import TypeVar
+from typing import TypeAlias, TypeVar
 
 import polars as pl
 
 AnyFrameT = TypeVar("AnyFrameT", pl.DataFrame, pl.LazyFrame )
+FrameOrLazy: TypeAlias = pl.DataFrame | pl.LazyFrame

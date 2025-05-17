0bdde008217cdfa844cebd3d9dc15dce347dee2b
MaksHess <max.hess@imls.uzh.ch>
2025-05-17 22:42:07 +0200

    type hints

--- zfish/multi_table/object_hierarchy.py
+++ zfish/multi_table/object_hierarchy.py
@@ -2,13 +2,13 @@
 import polars as pl
 import polars.selectors as cs
 
-from zfish.multi_table.schemas import sel
+from zfish.multi_table.schemas_v2 import sel
 
 
 def compute_cardinality_hierarchy(df_hierarchy, df_o):
     # Child cardinality (maximum one parent object -> 1 else -> m)
     df_cardinality_child = (
-        df_hierarchy.filter(pl.col("idx.label.parent") != 0)
+        df_hierarchy.filter(pl.col("label.parent") != 0)
         .group_by(sel.parent | sel.object_type)
         .len()
         .group_by(sel.object_type)
@@ -20,7 +20,7 @@ def compute_cardinality_hierarchy(df_hierarchy, df_o):
     )
     # parent cardinality -> 1:1, 1:m, m:1, m:m (should be mostly 1:m or 1:1 (parent:child))
     df_cardinality_parent = (
-        df_hierarchy.filter(pl.col("idx.label.parent") != 0)
+        df_hierarchy.filter(pl.col("label.parent") != 0)
         .group_by(sel.child | sel.object_type)
         .len()
         .group_by(sel.object_type)
@@ -40,34 +40,36 @@ def compute_cardinality_hierarchy(df_hierarchy, df_o):
                 df_cardinality_parent,
                 on=cs.expand_selector(df_cardinality_child, sel.object_type),
             ).select(sel.object_type, "cardinality_child", "cardinality_parent"),
-            left_on=["idx.o", "parents"],
-            right_on=["idx.o.child", "idx.o.parent"],
+            left_on=["o", "parents"],
+            right_on=["o.child", "o.parent"],
             how="full",
             coalesce=True,
         )
     )
     return (
         df_joined.select(
-            "idx.o",
+            "o",
             "hierarchy_level",
             "parents",
             pl.concat_str(
                 (pl.col("cardinality_child") > 1)
                 .cast(pl.String)
-                .replace({"true": "m", "false": "1"}, return_dtype=pl.String),
+                .replace_strict({"true": "m", "false": "1"}, return_dtype=pl.String),
                 (pl.col("cardinality_parent") > 1)
                 .cast(pl.String)
-                .replace({"true": "m", "false": "1"}, return_dtype=pl.String),
+                .replace_strict({"true": "m", "false": "1"}, return_dtype=pl.String),
                 separator=":",
             ).alias("cardinality"),
         )
-        .group_by("idx.o")
+        .group_by("o")
         .agg(pl.col("hierarchy_level").first(), pl.col("parents", "cardinality"))
         .with_columns(pl.col("parents", "cardinality").list.drop_nulls())
         .sort(
             "hierarchy_level",
             pl.col("parents").list.len(),
-            "idx.o",
+            "o",
             descending=[False, True, False],
         )
     )
+
+
